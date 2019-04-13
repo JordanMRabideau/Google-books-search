bf259e853d676f81e46f016cf15470504a007a60
Jordan Rabideau <jordanmrabideau@gmail.com>
2019-04-13 13:45:27 -0400

    more stuff

---
+++ client/build/precache-manifest.171d59f813ec3b2f76afb8f3993799e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9c584d7f7b4582e9451b",
+    "url": "/static/js/main.3a8b3578.chunk.js"
+  },
+  {
+    "revision": "9bf9496fe4fa0e7e282c",
+    "url": "/static/js/2.1fc8f037.chunk.js"
+  },
+  {
+    "revision": "9c584d7f7b4582e9451b",
+    "url": "/static/css/main.d273e046.chunk.css"
+  },
+  {
+    "revision": "caaa5f5fec8e717f43e9cf7226e97cc6",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae5042501062f680ac5d0b339a6736d9.js"
+  "/precache-manifest.171d59f813ec3b2f76afb8f3993799e9.js"
 );
 
 workbox.clientsClaim();
